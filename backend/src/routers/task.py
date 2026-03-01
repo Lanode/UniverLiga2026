c82e30ab4411748e02d7c44157b4662b6c13df53
Flays <flaystw@gmail.com>
2026-03-01 23:21:04 +0700

    Fix

--- backend/src/routers/task.py
+++ backend/src/routers/task.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy import select, func
+from sqlalchemy import select, func, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .. import schemas
@@ -20,13 +20,13 @@ async def read_task_users(
 ):
     current_user_id = current_user.id
     result_all_task_user = await db.execute(
-        select(TaskLink.task_id).where(TaskLink.user_id == current_user_id and TaskLink.user_id != current_user_id)
+        select(TaskLink.task_id).where((TaskLink.user_id == current_user_id))
     )
     if task_id not in result_all_task_user.scalars().all():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
 
     result = await db.execute(
-        select(User).join(TaskLink).filter(TaskLink.user_id == User.id).where(TaskLink.task_id == task_id)
+        select(User).join(TaskLink).filter(TaskLink.user_id == User.id).where((TaskLink.task_id == task_id)&(TaskLink.user_id != current_user_id))
     )
     users = result.scalars().all()
     return users
