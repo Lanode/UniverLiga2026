7fe52520833387a322cbc8bdc657b29caabc02c1
m.nazarenko <miklnazarenko@gmail.com>
2026-03-01 09:19:05 +0700

    add: statistics

---
+++ backend/statistics.py
@@ -0,0 +1,14 @@
+def marks():
+    return(0)
+
+def subcategories_marks():
+    return(0)
+
+def comments_pull():
+    return(0)
+
+def time_stat():
+    return(0)
+
+def command_stat():
+    return(0)
